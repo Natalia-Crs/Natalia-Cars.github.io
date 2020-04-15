19237ec2de0b5c6a6ddacc0473789cbdabfe7bf1
Natalia-Crs <63294061+Natalia-Crs@users.noreply.github.com>
2020-04-15 22:40:24 +0300

    Update MyScript.js

--- Tema10/MyScript.js
+++ Tema10/MyScript.js
@@ -1,1 +1,11 @@
+ function ask_for_movie() {
+Film=pompt(Care este filmul tau favorit? ") 
+document.getElementById.innerHTML ="filmul" + "meu" + "favorit" + "este" 
+" 
+} 
 
+function ask_for_serie() {
+Film=pompt(Care este serialul tau favorit? ") 
+document.getElementById.innerHTML ="serialul" + "meu" + "favorit" + "este" 
+" 
+} 
