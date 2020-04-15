5dc95e5174702a57478568bdefa4ab4f8cd95e69
Natalia-Crs <63294061+Natalia-Crs@users.noreply.github.com>
2020-04-15 22:41:56 +0300

    Update MyScript.js

--- Tema10/MyScript.js
+++ Tema10/MyScript.js
@@ -1,11 +1,11 @@
  function ask_for_movie() {
-film=pompt( Care este filmul tau favorit? ") 
+film=pompt(Care este filmul tau favorit? ") 
 document.getElementById.innerHTML ="filmul" + "meu" + "favorit" + "este" 
 " 
 } 
 
 function ask_for_serie() {
-serial=pompt( Care este serialul tau favorit? ") 
+serial=pompt(Care este serialul tau favorit? ") 
 document.getElementById.innerHTML ="serialul" + "meu" + "favorit" + "este" 
 " 
 } 
